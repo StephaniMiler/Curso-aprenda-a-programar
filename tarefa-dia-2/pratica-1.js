929128839e0437dd1eea06f8961bcba544b2a9e6
stephani <stephanisaramiler@gmail.com>
2022-08-30 19:40:41 -0300

    declaracao de variaveis code

--- tarefa-dia-2/pratica-1.js
+++ tarefa-dia-2/pratica-1.js
@@ -3,3 +3,16 @@ let email
 let profissao
 let idade
 let pais
+
+nome = "stephani"
+email= "stephanisaramiler@gmail.com"
+profissao = "aux adm"
+idade = "22"
+pais = "Japão"
+
+console.log(nome)
+console.log(email)
+console.log(profissao)
+console.log(idade)
+console.log(pais)
+
